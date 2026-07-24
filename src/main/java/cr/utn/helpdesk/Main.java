0ee29b6ce9a1a9c71a60695981883d11376e826a
Vanessa <erickavanessaamadorjimenezva@gmail.com>
2026-07-23 18:06:41 -0600

    se implemento el systema de modelo donde se creo incidencia, al correrlo funciona desde consola

--- src/main/java/cr/utn/helpdesk/Main.java
+++ src/main/java/cr/utn/helpdesk/Main.java
@@ -1,4 +1,27 @@
 package cr.utn.helpdesk;
 
+import cr.utn.helpdesk.enums.Categoria;
+import cr.utn.helpdesk.enums.Impacto;
+import cr.utn.helpdesk.enums.Urgencia;
+import cr.utn.helpdesk.model.Incidencia;
+
+import java.time.LocalDate;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Incidencia incidencia = new Incidencia(
+                1,
+                "No hay conexión a Internet",
+                "El equipo no puede acceder a la red desde esta mañana.",
+                Categoria.RED,
+                Impacto.ALTO,
+                Urgencia.ALTA,
+                LocalDate.now()
+        );
+
+        System.out.println(incidencia);
+
+    }
 }
